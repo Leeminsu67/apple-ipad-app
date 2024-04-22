16a1ae43b1173fc13d53ba25bd89cc3921957b1b
Leeminsu67 <alstn67@gmail.com>
2024-04-22 22:02:04 +0900

    검색바 기능 구현

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// 장바구니
 const basketStarterEl = document.querySelector(`header .basket-starter`);
 const basketEl = basketStarterEl.querySelector(`.basket`);
 const BASKETSHOW = `show`;
@@ -31,4 +32,28 @@ function showBasket() {
 
 function hideBasket() {
   basketEl.classList.remove(BASKETSHOW);
+}
+
+// 검색
+const headerEl = document.querySelector(`header`);
+const searchWrapEl = headerEl.querySelector(`.search-wrap`);
+const searchStarterEl = headerEl.querySelector(`.search-starter`);
+const searchCloserEl = searchWrapEl.querySelector(`.search-closer`);
+const searchShadowEl = searchWrapEl.querySelector(`.shadow`);
+
+searchStarterEl.addEventListener("click", showSearch);
+
+searchCloserEl.addEventListener("click", hideSearch);
+
+searchShadowEl.addEventListener("click", hideSearch);
+
+function showSearch() {
+  headerEl.classList.add(`searching`);
+  // document의 최상위 요소 선택(html)
+  document.documentElement.classList.add("fixed");
+}
+
+function hideSearch() {
+  headerEl.classList.remove(`searching`);
+  document.documentElement.classList.remove("fixed");
 }
